c793ab062ac2027344b34fa2d2f91eadfc94f182
VasiliySiniavin <vasinyavin@yandex.ru>
2024-06-03 14:11:32 +0300

    hidden service files

--- src/test/java/SelenideProject/Tests.java
+++ src/test/java/SelenideProject/Tests.java
@@ -1,7 +1,6 @@
 package SelenideProject;
 
 import com.codeborne.selenide.Configuration;
-import org.junit.Assert;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
